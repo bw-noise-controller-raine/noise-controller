7c536a52c7d6d07dc8efa650deffd835cfeb2888
angel prats <angelpratz@gmail.com>
2019-06-04 11:35:59 -0400

    created acreated reducers and working on switch

--- noisecontrol/src/actions/index.js
+++ noisecontrol/src/actions/index.js
@@ -7,9 +7,26 @@ export const FAILURE = 'FAILURE'
 export const login = creds => dispatch => {
   dispatch({ type: LOGIN });
   return axios
-    .post('https://noise-controller.herokuapp.com/api', creds)
+    .post('https://noise-controller.herokuapp.com/api/auth/login', creds)
     .then(res => {
       localStorage.setItem('token', res.data.payload);
       dispatch({ type: SUCCESS, payload: res.data.payload })
+    })
+}
+
+export const FETCHING_START = 'FETCHING_START';
+export const FETCHING_SUCCESS = 'FETCHING_SUCCESS';
+export const FETCHING_FAILURE = 'FETCHING_FAILURE';
+
+
+export const getlists = () => dispatch => {
+  dispatch({ type: FETCHING_CLASSES })
+  axios
+    .get('https://noise-controller.herokuapp.com/api/auth')
+    .then(res => {
+      dispatch({ type: FETCHING_SUCCESS, payload: res.data })
+    })
+    .catch(error => {
+      console.log(error)
     })
 }
---
+++ noisecontrol/src/reducers/index.js
@@ -0,0 +1,35 @@
+import { LOGIN, SUCCESS, FAILURE, FETCHING_START, FETCHING_SUCCESS, FETCHING_FAILURE } from '../actions'
+
+const initialSate = {
+  isLogging: false,
+  classes: [],
+  error: null,
+  fetchingData: false,
+  classesFetched: false,
+  savingClasses: false,
+  classesSaved: false,
+  updatingClasses: false,
+  classesUpdated: false,
+  deletingClasses: false,
+  classesDeleted: false,
+};
+
+export const groupReducer = (state = initialState, action) => {
+  switch (action.type) {
+    case LOGIN:
+      return {
+        ...state,
+        isLoggingIn: true,
+        error: ''
+      };
+    case SUCCESS:
+      return {
+        ...state,
+        isLoggedIn: true
+      }
+    case FETCHING_START:
+      return { ...state, fetchingStart: true, classes: action.payload };
+
+    case FETCHING_SUCCESS:
+      console.log(action.payload);
+      return { ...state, fetchingClasses: false, classes: action.payload };
