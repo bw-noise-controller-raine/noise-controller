16f9a2e1e6583a4f6b76f6c2c8487df250458bc0
angel prats <angelpratz@gmail.com>
2019-06-04 11:59:12 -0400

    installed redux

--- noisecontrol/src/index.js
+++ noisecontrol/src/index.js
@@ -3,6 +3,7 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import { BrowseRouter as Router } from 'react-render-dom';
+import { Provider } from 'react-redux'
 
 ReactDOM.render(
   <Router>
